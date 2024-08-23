047f6f647228fbdd30832faeba56e4984845cd0e
TrungNguyenHP <152580377+TrungNguyenHP@users.noreply.github.com>
2024-08-23 17:45:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ require_once(ROOT.'/config/settings.php');
 
 require_once(ROOT . '/components/functions.php');
 
-//$pdo = Connection::connect();
+$pdo = Connection::connect();
  
 $router = new Router();
 $router->run();
